9f0592c1a3275fa37bd4f81d317349ee048356d3
aksaabdulrazaq1 <s2023376026@umt.edu.pk>
2026-01-23 07:36:34 +0000

    Generating index.html with distill_path

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home # This must exist!
+from django.conf import settings
+from django.conf.urls.static import static
+from django_distill import distill_path # Import this!
+from .views import home
+
+# This function tells distill what to name the file (index.html)
+def get_index():
+    return None
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'), # This is what removes the rocket
+    # Change 'path' to 'distill_path' for the homepage
+    distill_path('', home, name='home', distill_func=get_index),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
