f477e89c836d3bca8c53f28e87e31807c6947d6c
aksaabdulrazaq1 <s2023376026@umt.edu.pk>
2026-01-26 10:04:12 +0000

    Final Portfolio Update: Projects and Skills synced

--- education/models.py
+++ education/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Degree(models.Model):
-    title = models.CharField(max_length=100)  # e.g. BS Computer Science
-    institution = models.CharField(max_length=100) # e.g. Your University
+    title = models.CharField(max_length=100)
+    institution = models.CharField(max_length=100)
     year_start = models.IntegerField()
-    year_end = models.IntegerField(blank=True, null=True) # Can be empty if current
+    year_end = models.IntegerField(blank=True, null=True)
     grade = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
--- myportfolio/views.py
+++ myportfolio/views.py
@@ -5,10 +5,21 @@ from skills.models import Skill
 from experience.models import Project
 
 def home(request):
+    # 1. Fetch data from database
+    bios = Bio.objects.all()
+    degrees = Degree.objects.all()
+    skills = Skill.objects.all()
+    projects = Project.objects.all()
+
+    # 2. Put data in context
     context = {
-        'bio': Bio.objects.first(),
-        'degrees': Degree.objects.all(),
-        'skills': Skill.objects.all(),
-        'projects': Project.objects.all(),
+        'bios': bios,
+        'degrees': degrees,
+        'skills': skills,
+        'projects': projects,
     }
+    
+    # 3. Send to template
     return render(request, 'index.html', context)
+
+# MAKE SURE THERE IS NOTHING WRITTEN BELOW THIS LINE
